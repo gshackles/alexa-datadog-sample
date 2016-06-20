810339808be99f2d892ddfd9edf283c98dbeab09
Greg Shackles <gshackles@gmail.com>
2016-06-20 09:35:54 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# alexa-datadog-sample
+
+This is a simple example of integrating Datadog with an Amazon Echo. This is written on top of the [Serverless framework](http://serverless.com/) to simplify development and deployment, but that's not important to the implementation.
+
+Currently the only supported query is to ask for CPU levels, which will list out the current CPU values for all hosts in your account. For example, saying:
+
+> Alexa, ask Datadog to check the CPU
+
+will result in a response along the lines of:
+
+> Here are the current CPU loads. Gregs MacBook Pro is at 7%. Gregs iMac is at 4%
+
+<iframe width="100%" height="80" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/269945578&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+
+# Interaction Model
+
+Here's an example of how to set up an interaction model for this skill. 
+
+## Intent Schema
+
+```language-json
+{
+  "intents": [
+    {
+      "intent": "QueryIntent",
+      "slots": [
+        {
+          "name": "Query",
+          "type": "QUERY_LIST"
+        }
+      ]
+    }
+  ]
+}
+```
+
+## Custom Slot Types
+
+Type       | Values	
+---------- | ------
+QUERY_LIST | cpu
+
+## Sample Utterances
+
+```
+QueryIntent query {Query}
+QueryIntent check {Query}
+QueryIntent to query {Query}
+QueryIntent to check {Query}
+QueryIntent to query the {Query}
+QueryIntent to check the {Query}
+```
+
+# Additional Configuration
+
+You'll also need to supply your Datadog API and app keys in order for the queries to work properly. These should be add as Serverless variables, such as through the `_meta/variables/s-veriables-dev.json` file. Here's the format you'll want to use in that file:
+
+```language-json
+{
+  "datadogApiKey": "your-api-key-here",
+  "datadogAppKey": "your-app-key-here"
+}
+```
---
+++ functions/queryDatadog/handler.js
@@ -0,0 +1,63 @@
+import process from 'process';
+import dogapi from 'dogapi';
+import Promise from 'bluebird';
+import {buildSpeechletResponse, buildResponse} from '../../lib/helpers';
+
+const queryDatadog = Promise.promisify(dogapi.metric.query);
+
+function queryCPU() {
+  const now = parseInt(new Date().getTime() / 1000);
+  const then = now - 300;
+  const query = 'system.cpu.user{*}by{host}';
+  
+  return queryDatadog(then, now, query)
+    .then(res => res.series.map(reading => ({
+      name: reading.scope
+                   .replace(/^host:/i, '')
+                   .replace(/(\..*$)/i, '')
+                   .replace(/\W/g, ' '),
+      value: reading.pointlist[reading.pointlist.length - 1][1]
+    })));
+}
+
+function processIntent(intentRequest, session) {
+  const intent = intentRequest.intent;
+
+  if (intent.name === 'QueryIntent') {
+    const querySlot = intent.slots.Query;
+    
+    if (querySlot.value && querySlot.value.toLowerCase() === 'cpu') {
+      return queryCPU().then(readings => {
+        const hostSpeechFragments = readings.map(reading =>
+          `${reading.name} is at ${reading.value}%`).join('. ');
+        const speechOutput = `Here are the current CPU loads. ${hostSpeechFragments}`;
+        
+        return buildSpeechletResponse(
+          'CPU Load', 
+          speechOutput,
+          null, 
+          true);
+      });
+    }
+  }
+  
+  return Promise.resolve(buildSpeechletResponse(
+    'Datadog Query',
+    'Sorry, I don\'t know that query',
+    null,
+    true
+  ));
+}
+
+module.exports.handler = function(event, context, callback) {
+  dogapi.initialize({
+    api_key: process.env.DATADOG_API_KEY,
+    app_key: process.env.DATADOG_APP_KEY
+  });
+  
+  if (event.request.type === 'IntentRequest') {
+    processIntent(event.request, event.session)
+      .then(speechletResponse =>
+        context.succeed(buildResponse({}, speechletResponse)));
+  }
+};
